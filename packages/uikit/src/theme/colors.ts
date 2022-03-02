1b1c9b00f0f15c3199abc3ec4cac28baead91fec
Kazim Akgul <kazimakgul@gmail.com>
2022-03-02 02:19:04 -0800

    grey background

--- packages/uikit/src/theme/colors.ts
+++ packages/uikit/src/theme/colors.ts
@@ -52,7 +52,7 @@ export const darkColors: Colors = {
   ...baseColors,
   ...additionalColors,
   secondary: "#9A6AFF",
-  background: "#08060B",
+  background: "#1E1D20",
   backgroundDisabled: "#3c3742",
   backgroundAlt: "#27262c",
   backgroundAlt2: "rgba(39, 38, 44, 0.7)",
