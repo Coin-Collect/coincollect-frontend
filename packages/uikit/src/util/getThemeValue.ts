62cad139adbed61e66af95486526353de602712f
r <100163255+r0b0roach@users.noreply.github.com>
2023-03-17 14:28:44 +0300

    Update getThemeValue.ts

--- packages/uikit/src/util/getThemeValue.ts
+++ packages/uikit/src/util/getThemeValue.ts
@@ -1,6 +1,7 @@
 import get from "lodash/get";
 import { DefaultTheme } from "styled-components";
 
+// @ts-ignore
 const getThemeValue = (theme: DefaultTheme, path: string, fallback?: string | number): string =>
   get(theme, path, fallback);
 
