15a368a26ed04041c31490513e63bf4e311e6679
r <100163255+r0b0roach@users.noreply.github.com>
2023-03-18 01:35:53 +0300

    link

--- packages/uikit/src/components/CakePrice/CakePrice.tsx
+++ packages/uikit/src/components/CakePrice/CakePrice.tsx
@@ -26,7 +26,7 @@ const PriceLink = styled.a`
 const CakePrice: React.FC<Props> = ({ cakePriceUsd, color = "textSubtle" }) => {
   return cakePriceUsd ? (
     <PriceLink
-      href="https://polygonscan.com/token/0x56633733fc8baf9f730ad2b6b9956ae22c6d4148" // Must go to swap page
+      href="/swap?outputCurrency=0x56633733fc8BAf9f730AD2b6b9956Ae22c6d4148"
       target="_blank"
     >
       <LogoRound width="24px" mr="8px" />
