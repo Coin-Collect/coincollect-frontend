74a8d89bb6ebf056f99c2cf2233c19d743144056
r <100163255+r0b0roach@users.noreply.github.com>
2023-06-26 19:45:00 +0300

    Update CardHeading.tsx

--- src/views/NftFarms/components/FarmCard/CardHeading.tsx
+++ src/views/NftFarms/components/FarmCard/CardHeading.tsx
@@ -1,13 +1,15 @@
 import styled from 'styled-components'
-import { Tag, Flex, Heading, Skeleton, TokenImage } from '@pancakeswap/uikit'
+import { Tag, Flex, Heading, Skeleton, TokenImage, ProfileAvatar, CardBody } from '@pancakeswap/uikit'
 import { Token } from '@coincollect/sdk'
 import { FarmAuctionTag, CoreTag } from 'components/Tags'
+import Image from 'next/image'
 
 
 export interface ExpandableSectionProps {
   lpLabel?: string
   multiplier?: string
   nftToken?: string
+  disabled?: boolean
 }
 
 const Wrapper = styled(Flex)`
@@ -20,24 +22,35 @@ const MultiplierTag = styled(Tag)`
   margin-left: 4px;
 `
 
+const StyledImage = styled(Image)`
+  border-radius: 4px;
+`
 
-const CardHeading: React.FC<ExpandableSectionProps> = ({ lpLabel, multiplier, nftToken }) => {
+export const CollectionAvatar = styled(ProfileAvatar)`
+  left: 0;
+  position: absolute;
+  top: -32px;
+  border: 4px white solid;
+`
+
+const CardHeading: React.FC<ExpandableSectionProps> = ({ lpLabel, multiplier, nftToken, disabled= false }) => {
   return (
-    <Wrapper justifyContent="space-between" alignItems="center" mb="12px">
-      <TokenImage src={`/images/tokens/${nftToken}.svg`} width={64} height={64} />
-      <Flex flexDirection="column" alignItems="flex-end">
-        <Heading mb="4px">{lpLabel.split(' ')[0]}</Heading>
-        {/*
-        <Flex justifyContent="center">
-          {multiplier ? (
-            <MultiplierTag variant="secondary">{multiplier}</MultiplierTag>
-          ) : (
-            <Skeleton ml="4px" width={42} height={28} />
-          )}
-        </Flex>
-       */}
+    <CardBody p="0px">
+      <StyledImage src={"https://coincollect.org/assets/images/clone/banners/bannerBronzeSm.png"} height={125} width={375} />
+      <Flex
+        position="relative"
+        height="65px"
+        justifyContent="center"
+        alignItems="flex-end"
+        py="8px"
+        flexDirection="column"
+      >
+        <CollectionAvatar src={"https://coincollect.org/assets/images/clone/nft350.png"} width={96} height={96} />
+        <Heading color={disabled ? 'textDisabled' : 'body'} as="h3" mb={'8px'}>
+          {/*lpLabel.split(' ')[0]*/ "CoinCollect Starter NFT"}
+        </Heading>
       </Flex>
-    </Wrapper>
+    </CardBody>
   )
 }
 
