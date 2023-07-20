d9223ca8f03ec6ff1d2adb1c6696380603031da9
r <100163255+r0b0roach@users.noreply.github.com>
2023-07-20 12:48:12 +0300

    new claim

--- src/config/constants/claim.tsx
+++ src/config/constants/claim.tsx
@@ -15,6 +15,19 @@ const claims: any[] = [
     rewardTokenAddress: '0xadF053697C602C76feB56d4E4C7d28c3E1Bd1075',
     projectSite: 'https://app.coincollect.org'
   },
+  {
+    name: 'LotShare Rewards',
+    description: <Trans>Lotshare NFT holders, your share of the reward pool awaits! Enter the realm of exclusive benefits and claim what you rightfully deserve. Lotshare - where ownership translates into rewards!</Trans>,
+    imageLink: 'https://lotshare.app/images/artwork/06.jpg',
+    rewardToken: 'Lot',
+    requiredToken: 'Lot NFT', 
+    baseAmount: 50,
+    nftLimit: 5,
+    totalReward: 100000,
+    isFinished: false,
+    rewardTokenAddress: '0x877dF062E88C8AB14FA60c945474641194203BFb',
+    projectSite: 'https://lotshare.app/'
+  },
   /*
   {
     name: 'Holder Party',
