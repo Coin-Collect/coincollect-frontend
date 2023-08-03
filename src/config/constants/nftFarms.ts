603559c1201cd41f9c6820d2e7188266535a33af
r <100163255+r0b0roach@users.noreply.github.com>
2023-08-03 15:23:11 +0300

    Update nftFarms.ts

--- src/config/constants/nftFarms.ts
+++ src/config/constants/nftFarms.ts
@@ -81,7 +81,7 @@ const nftFarms: SerializedNftFarmConfig[] = [
     },
     contractAddresses: {
       137: '0x2b0dcF4f55A6f19aF410D7BD51bbfAb499Bb5C84',
-      80001: '0x02905b3528f0D737BB324A202B7F79eA0e9d808d',
+      80001: '0x02905b3528f0D737BB324A202B7F79eA0e9d808a',
     },
     tokenPerBlock: '0.0077',
     participantThreshold: 100,
@@ -93,9 +93,84 @@ const nftFarms: SerializedNftFarmConfig[] = [
     //supportedCollectionPids: [1,2],
     mainCollectionWeight: '1',
     performanceFee: '0',
-    projectLink: 'https://lotshare.app/',
+    projectLink: 'https://coincollect.org/',
     banner: "https://coincollect.org/assets/images/clone/banner-lg.png",
     avatar: "https://coincollect.org/assets/images/clone/nft350.png",
+  },
+  {
+    pid: 7,
+    lpSymbol: 'CoinCollect Bronze NFT',
+    nftAddresses: {
+      137: '0x0B8E7D22CE826f1228a82525b8779dBdD9E24B80',
+      80001: '0x2c65d5355813D3E1f86d1c9b25DCFF367bBd913D' 
+    },
+    contractAddresses: {
+      137: '0x2b0dcF4f55A6f19aF410D7BD51bbfAb499Bb5C84',
+      80001: '0x02905b3528f0D737BB324A202B7F79eA0e9d808a',
+    },
+    tokenPerBlock: '0.0230',
+    participantThreshold: 60,
+    isFinished: false,
+    earningToken: serializedTokens.collect,
+    sideRewards: [
+      {token: 'SHIB', percentage: 200},
+    ],
+    //supportedCollectionPids: [1,2],
+    mainCollectionWeight: '1',
+    performanceFee: '0',
+    projectLink: 'https://coincollect.org/',
+    banner: "https://coincollect.org/assets/images/clone/banners/bannerBronzeSm.png",
+    avatar: "https://coincollect.org/assets/images/clone/banners/profileBronze.png",
+  },
+  {
+    pid: 8,
+    lpSymbol: 'CoinCollect Silver NFT',
+    nftAddresses: {
+      137: '0x0a846Dd40152d6fE8CB4DE4107E0b063B6D6b3F9',
+      80001: '0x0a846Dd40152d6fE8CB4DE4107E0b063B6D6b3F9' 
+    },
+    contractAddresses: {
+      137: '0x2b0dcF4f55A6f19aF410D7BD51bbfAb499Bb5C84',
+      80001: '0x02905b3528f0D737BB324A202B7F79eA0e9d808a',
+    },
+    tokenPerBlock: '0.0224',
+    participantThreshold: 25,
+    isFinished: false,
+    earningToken: serializedTokens.collect,
+    sideRewards: [
+      {token: 'SHIB', percentage: 143},
+    ],
+    //supportedCollectionPids: [1,2],
+    mainCollectionWeight: '1',
+    performanceFee: '0',
+    projectLink: 'https://coincollect.org/',
+    banner: "https://coincollect.org/assets/images/clone/banners/bannerSilverSm.png",
+    avatar: "https://coincollect.org/assets/images/clone/banners/profileSilver.png",
+  },
+  {
+    pid: 9,
+    lpSymbol: 'CoinCollect Gold NFT',
+    nftAddresses: {
+      137: '0x117D6870e6dE9faBcB40C34CceDD5228C63e3a1e',
+      80001: '0x117D6870e6dE9faBcB40C34CceDD5228C63e3a1e'
+    },
+    contractAddresses: {
+      137: '0x2b0dcF4f55A6f19aF410D7BD51bbfAb499Bb5C84',
+      80001: '0x02905b3528f0D737BB324A202B7F79eA0e9d808a',
+    },
+    tokenPerBlock: '0.0306',
+    participantThreshold: 15,
+    isFinished: false,
+    earningToken: serializedTokens.collect,
+    sideRewards: [
+      {token: 'SHIB', percentage: 125},
+    ],
+    //supportedCollectionPids: [1,2],
+    mainCollectionWeight: '1',
+    performanceFee: '0',
+    projectLink: 'https://coincollect.org/',
+    banner: "https://coincollect.org/assets/images/clone/banners/bannerGoldSm.png",
+    avatar: "https://coincollect.org/assets/images/clone/banners/profileGold.png",
   },
 ].filter((f) => !!f.nftAddresses[ChainId.POLYGON])
 
