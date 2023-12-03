4ea5937897eff019f382fd1cb2e4edb98e38186c
r <100163255+r0b0roach@users.noreply.github.com>
2023-12-03 17:51:07 +0300

    Update farms.ts

--- src/config/constants/farms.ts
+++ src/config/constants/farms.ts
@@ -114,7 +114,7 @@ const farms: SerializedFarmConfig[] = [
       97: '0x3ed8936cAFDF85cfDBa29Fbe5940A5b0524824F4',
       56: '0x0eD7e52944161450477ee417DE9Cd3a859b14fD0',
       137: '0x9556C3c45333476e0D2466CfBBE8f001055BDc41',
-      80001: '0xf228A55fFCD281b5a245D48f5677870313381c15' // CHANGE_ADDRESS:Pair
+      80001: '0x43Db09aa003A824A04BFd7555D92Ea7EB3F85d11' // CHANGE_ADDRESS:Pair
     },
     token: serializedTokens.dj,
     quoteToken: serializedTokens.collect,
