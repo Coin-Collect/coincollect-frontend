ed542d411e472028145dec4b685ca98397f4dd47
r <100163255+r0b0roach@users.noreply.github.com>
2024-05-18 00:15:19 +0300

    External Link Icon

--- packages/uikit/src/widgets/Menu/components/MenuLink.tsx
+++ packages/uikit/src/widgets/Menu/components/MenuLink.tsx
@@ -1,5 +1,6 @@
 import NextLink from "next/link";
 import React, { AnchorHTMLAttributes } from "react";
+import { OpenNewIcon } from "../../../components/Svg";
 
 const MenuLink: React.FC<AnchorHTMLAttributes<HTMLAnchorElement>> = ({ href, ...otherProps }) => {
   const isHttpLink = href?.startsWith("http");
@@ -7,7 +8,14 @@ const MenuLink: React.FC<AnchorHTMLAttributes<HTMLAnchorElement>> = ({ href, ...
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   const Tag: any = isHttpLink ? "a" : NextLink;
   const props = { href };
-  return <Tag {...props} {...otherProps} />;
+  return (
+    <Tag {...props} {...otherProps}>
+      <>
+      {otherProps.children}
+      {isHttpLink && <OpenNewIcon ml={10} />} {/* isHttpLink, add external icon */}
+      </>
+    </Tag>
+  );
 };
 
 export default MenuLink;
