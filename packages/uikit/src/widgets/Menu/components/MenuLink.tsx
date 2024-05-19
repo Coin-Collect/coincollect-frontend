37d5de1cf3a669e3413b16501223ba0da8f939a5
r <100163255+r0b0roach@users.noreply.github.com>
2024-05-19 13:02:22 +0300

    MenuLink Fixed

--- packages/uikit/src/widgets/Menu/components/MenuLink.tsx
+++ packages/uikit/src/widgets/Menu/components/MenuLink.tsx
@@ -5,17 +5,19 @@ import { OpenNewIcon } from "../../../components/Svg";
 const MenuLink: React.FC<AnchorHTMLAttributes<HTMLAnchorElement>> = ({ href, ...otherProps }) => {
   const isHttpLink = href?.startsWith("http");
 
+
+  otherProps.children = isHttpLink ? (
+    <>
+      {otherProps.children}
+      {<OpenNewIcon ml={10} />} {/* isHttpLink, add external icon */}
+    </>
+  ) : otherProps.children;
+
+
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   const Tag: any = isHttpLink ? "a" : NextLink;
   const props = { href };
-  return (
-    <Tag {...props} {...otherProps}>
-      <>
-      {otherProps.children}
-      {isHttpLink && <OpenNewIcon ml={10} />} {/* isHttpLink, add external icon */}
-      </>
-    </Tag>
-  );
+  return <Tag {...props} {...otherProps} />;
 };
 
 export default MenuLink;
