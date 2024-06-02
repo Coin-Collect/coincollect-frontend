e71e280929cb7253cec5567e4eafb8e24dba7c23
r <100163255+r0b0roach@users.noreply.github.com>
2024-06-02 13:46:56 +0300

    Update DepositModal.tsx

--- src/views/NftFarms/components/DepositModal.tsx
+++ src/views/NftFarms/components/DepositModal.tsx
@@ -1,37 +1,38 @@
 import BigNumber from 'bignumber.js'
-import { useCallback, useMemo, useState } from 'react'
+import { useCallback, useState } from 'react'
 import styled from 'styled-components'
-import { Flex, Text, Button, Modal, LinkExternal, CalculateIcon, IconButton, Skeleton, AutoRenewIcon } from '@pancakeswap/uikit'
+import { Flex, Text, Button, Modal, LinkExternal, AutoRenewIcon, ModalBody } from '@pancakeswap/uikit'
 import { ModalActions, ModalInput } from 'components/Modal'
 import RoiCalculatorModal from 'components/RoiCalculatorModal'
 import { useTranslation } from 'contexts/Localization'
-import { getFullDisplayBalance, formatNumber } from 'utils/formatBalance'
-import { getInterestBreakdown } from 'utils/compoundApyHelpers'
 import { RoundedImage } from 'views/Nft/market/Collection/IndividualNFTPage/shared/styles'
 import { useNftsForCollectionAndAddress } from 'views/Nft/market/hooks/useNftsForCollectionAndAddress'
 import CircleLoader from 'components/Loader/CircleLoader'
 import NoNftsImage from 'views/Nft/market/components/Activity/NoNftsImage'
+import useTheme from 'hooks/useTheme'
 
-const AnnualRoiContainer = styled(Flex)`
-  cursor: pointer;
-`
-
-const AnnualRoiDisplay = styled(Text)`
-  width: 72px;
-  max-width: 72px;
-  overflow: hidden;
-  text-align: right;
-  text-overflow: ellipsis;
-`
 
 const NftBox = styled(RoundedImage)`
     border-radius: 6px;
+    position: relative;
+    background-color: #ffffff;
+    &:hover {
+        border: 2px solid #cac7c8;
+    }
 `
-
 const SelectedNftBox = styled(RoundedImage)`
     border-radius: 6px;
-    box-shadow: 0 0 10px 4px #e91e63;
+    background-color: #f8bbd0;
+    border: 2px solid #e91e63;
 `
+
+const Wrapper = styled(Flex)`
+  background: ${props => props.theme.colors.background};
+  border-radius: 16px;
+  max-height: 400px;
+  overflow-y: auto;
+  padding: 15px;
+`;
 
 interface DepositModalProps {
   max: BigNumber
@@ -68,29 +69,13 @@ const DepositModal: React.FC<DepositModalProps> = ({
 }) => {
   const [val, setVal] = useState('')
   const [pendingTx, setPendingTx] = useState(false)
-  const [showRoiCalculator, setShowRoiCalculator] = useState(false)
   const [selectedNftList, setSelectedNftList] = useState<{ collectionAddress: string; tokenId: number }[]>([]);
   const { t } = useTranslation()
-  const fullBalance = useMemo(() => {
-    return getFullDisplayBalance(max)
-  }, [max])
+  const { theme } = useTheme()
 
   const isStakeLimitReached = stakingLimit.toNumber() > 0 && stakedBalance.toNumber() + selectedNftList.length > stakingLimit.toNumber()
 
-  const lpTokensToStake = new BigNumber(val)
-
-
-  const usdToStake = lpTokensToStake.times(lpPrice)
-
-  const interestBreakdown = getInterestBreakdown({
-    principalInUSD: !lpTokensToStake.isNaN() ? usdToStake.toNumber() : 0,
-    apr,
-    earningTokenPrice: cakePrice.toNumber(),
-  })
-
-  const annualRoi = cakePrice.times(interestBreakdown[3])
-  const annualRoiAsNumber = annualRoi.toNumber()
-  const formattedAnnualRoi = formatNumber(annualRoiAsNumber, annualRoi.gt(10000) ? 0 : 2, annualRoi.gt(10000) ? 0 : 2)
+  
 
   const { nfts, isLoading, error, revalidateNfts } = useNftsForCollectionAndAddress(pid)
   const handleRefresh = () => {
@@ -111,8 +96,8 @@ const DepositModal: React.FC<DepositModalProps> = ({
         onClick={() => handleSelectNft(nft.collectionAddress, nft.tokenId)}
         src={nft.image}
         height={90}
-        width={68}
-        m="8px"
+        width={90}
+        m="3px"
       />
     ) : (
       <NftBox
@@ -120,8 +105,8 @@ const DepositModal: React.FC<DepositModalProps> = ({
         onClick={() => handleSelectNft(nft.collectionAddress, nft.tokenId)}
         src={nft.image}
         height={90}
-        width={68}
-        m="8px"
+        width={90}
+        m="3px"
       />
     );
   });
@@ -147,99 +132,83 @@ const DepositModal: React.FC<DepositModalProps> = ({
 
 
 
-
-  if (showRoiCalculator) {
-    return (
-      <RoiCalculatorModal
-        linkLabel={t('Get %symbol%', { symbol: lpLabel })}
-        stakingTokenBalance={stakedBalance.plus(max)}
-        stakingTokenSymbol={tokenName}
-        stakingTokenPrice={lpPrice.toNumber()}
-        earningTokenPrice={cakePrice.toNumber()}
-        apr={apr}
-        multiplier={multiplier}
-        displayApr={displayApr}
-        linkHref={addLiquidityUrl}
-        isFarm
-        initialValue={val}
-        onBack={() => setShowRoiCalculator(false)}
-      />
-    )
-  }
-
   return (
-    <Modal title={t('Select NFTs to Stake')} onDismiss={onDismiss}>
+    <Modal title={t('Select NFTs to Stake')} headerBackground={theme.colors.gradients.bubblegum} onDismiss={onDismiss}>
+      <ModalBody maxWidth="620px">
 
-      {stakingLimit.gt(0) && (
-        <Text color="secondary" bold mb="5px" style={{ textAlign: 'center' }} fontSize="20px">
-          {t('Max stake: %amount% NFT', {
-            amount: stakingLimit.toNumber(),
-            token: lpLabel,
-          })}
-        </Text>
-      )}
-
-      {isStakeLimitReached && (
-        <Text color="warning" fontSize="20px" style={{ textAlign: 'center' }} mb="10px">
-          {t('Max Stake Limit Reached!')}
-        </Text>
-      )}
-
-      {nftList.length === 0 && !isLoading && !error ? (
-        <Flex p="24px" flexDirection="column" alignItems="center">
-          <NoNftsImage />
-          <Text pt="8px" bold>
-            {t('No NFTs found')}
+        {stakingLimit.gt(0) && (
+          <Text color="secondary" bold mb="5px" style={{ textAlign: 'center' }} fontSize="20px">
+            {t('Max stake: %amount% NFT', {
+              amount: stakingLimit.toNumber(),
+              token: lpLabel,
+            })}
           </Text>
-        </Flex>
-      ) : nftList.length > 0 ? (
-        <Flex flexWrap="wrap" justifyContent="space-evenly">
-          {nftList}
-        </Flex>
-      ) : error ? (
-        <Flex p="24px" flexDirection="column" alignItems="center">
-          <Button variant="light" onClick={handleRefresh} width="100%">
-            {t('Retry')}
+        )}
+
+        {isStakeLimitReached && (
+          <Text color="warning" fontSize="20px" style={{ textAlign: 'center' }} mb="10px">
+            {t('Max Stake Limit Reached!')}
+          </Text>
+        )}
+
+        <Wrapper>
+          {nftList.length === 0 && !isLoading && !error ? (
+            <Flex p="24px" flexDirection="column" alignItems="center">
+              <NoNftsImage />
+              <Text pt="8px" bold>
+                {t('No NFTs found')}
+              </Text>
+            </Flex>
+          ) : nftList.length > 0 ? (
+            <Flex flexWrap="wrap" justifyContent="space-evenly">
+              {nftList}
+            </Flex>
+          ) : error ? (
+            <Flex p="24px" flexDirection="column" alignItems="center">
+              <Button variant="light" onClick={handleRefresh} width="100%">
+                {t('Retry')}
+              </Button>
+              <Text pt="8px" fontSize='15px'>
+                {t('There was a temporary network issue while fetching NFTs.')}
+              </Text>
+              <Text pt="8px" fontSize='15px'>
+                {t('Please wait a few seconds and press the retry button.')}
+              </Text>
+            </Flex>
+          ) : (
+            <Flex p="24px" flexDirection="column" alignItems="center">
+              <CircleLoader size="30px" />
+              <Text mt={1}>NFTs will be listed shortly...</Text>
+            </Flex>
+          )}
+
+        </Wrapper>
+
+        <ModalActions>
+          <Button variant="secondary" onClick={onDismiss} width="100%" disabled={pendingTx}>
+            {t('Cancel')}
           </Button>
-          <Text pt="8px" fontSize='15px'>
-            {t('There was a temporary network issue while fetching NFTs.')}
-          </Text>
-          <Text pt="8px" fontSize='15px'>
-            {t('Please wait a few seconds and press the retry button.')}
-          </Text>
-        </Flex>
-      ) : (
-        <Flex p="24px" flexDirection="column" alignItems="center">
-          <CircleLoader size="30px" />
-          <Text mt={1}>NFTs will be listed shortly...</Text>
-        </Flex>
-      )}
-
-
-      <ModalActions>
-        <Button variant="secondary" onClick={onDismiss} width="100%" disabled={pendingTx}>
-          {t('Cancel')}
-        </Button>
-        <Button
-          width="100%"
-          isLoading={pendingTx}
-          endIcon={pendingTx ? <AutoRenewIcon spin color="currentColor" /> : null}
-          disabled={
-            selectedNftList.length === 0 || isStakeLimitReached
-          }
-          onClick={async () => {
-            setPendingTx(true)
-            await onConfirm(selectedNftList)
-            onDismiss?.()
-            setPendingTx(false)
-          }}
-        >
-          {pendingTx ? t('Confirming') : t('Confirm')}
-        </Button>
-      </ModalActions>
-      <LinkExternal href={addLiquidityUrl} style={{ alignSelf: 'center' }}>
-        {t('Get %symbol%', { symbol: tokenName })}
-      </LinkExternal>
+          <Button
+            width="100%"
+            isLoading={pendingTx}
+            endIcon={pendingTx ? <AutoRenewIcon spin color="currentColor" /> : null}
+            disabled={
+              selectedNftList.length === 0 || isStakeLimitReached
+            }
+            onClick={async () => {
+              setPendingTx(true)
+              await onConfirm(selectedNftList)
+              onDismiss?.()
+              setPendingTx(false)
+            }}
+          >
+            {pendingTx ? t('Confirming') : t('Confirm')}
+          </Button>
+        </ModalActions>
+        <LinkExternal href={addLiquidityUrl} style={{ alignSelf: 'center' }}>
+          {t('Get %symbol%', { symbol: tokenName })}
+        </LinkExternal>
+      </ModalBody>
     </Modal>
   )
 }
