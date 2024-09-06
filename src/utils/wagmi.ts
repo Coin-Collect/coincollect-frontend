33c381ed9b35633c845cf188d23846c0b196ba7c
r <100163255+r0b0roach@users.noreply.github.com>
2024-09-06 18:05:20 +0300

    Create wagmi.ts

---
+++ src/utils/wagmi.ts
@@ -0,0 +1,33 @@
+import { defaultWagmiConfig } from '@web3modal/wagmi/react/config'
+import memoize from 'lodash/memoize'
+import { polygon, polygonAmoy } from 'wagmi/chains'
+
+// Get projectId from https://cloud.walletconnect.com
+export const projectId = 'e0c7decec4ed90ec17fd3c5f3cba1c4c'
+
+if (!projectId) throw new Error('Project ID is not defined')
+
+export const metadata = {
+  name: 'CoinCollect',
+  description: 'AppKit Example',
+  url: 'https://app.coincollect.org/', // origin must match your domain & subdomain
+  icons: ['https://avatars.githubusercontent.com/u/37784886']
+}
+
+// Create wagmiConfig
+const chains = [polygon, polygonAmoy] as const
+export const config = defaultWagmiConfig({
+  chains,
+  projectId,
+  metadata,
+  ssr: true,
+  auth: {
+    email: true, // default to true
+    socials: undefined,
+    showWallets: false, // default to true
+    walletFeatures: false // default to true
+  }
+})
+
+export const CHAIN_IDS = chains.map((c) => c.id)
+export const isChainSupported = memoize((chainId: number) => (CHAIN_IDS as number[]).includes(chainId))
