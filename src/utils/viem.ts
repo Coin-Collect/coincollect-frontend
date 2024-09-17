051f7ef3537245da5cae7ef26744be395ec56561
r <100163255+r0b0roach@users.noreply.github.com>
2024-09-17 21:23:27 +0300

    Create viem.ts

---
+++ src/utils/viem.ts
@@ -0,0 +1,89 @@
+import { ChainId } from '@coincollect/sdk'
+import first from 'lodash/first'
+import { PublicClient, createPublicClient, fallback, http } from 'viem'
+import { polygon } from 'viem/chains'
+
+import { CHAINS } from 'config/chains'
+import { PUBLIC_NODES } from 'config/nodes'
+
+export type CreatePublicClientParams = {
+  transportSignal?: AbortSignal
+}
+
+export function createViemPublicClients({ transportSignal }: CreatePublicClientParams = {}) {
+  return CHAINS.reduce((prev, cur) => {
+    return {
+      ...prev,
+      [cur.id]: createPublicClient({
+        chain: cur,
+        transport: fallback(
+          (PUBLIC_NODES[cur.id] as string[]).map((url) =>
+            http(url, {
+              timeout: 10_000,
+              fetchOptions: {
+                signal: transportSignal,
+              },
+            }),
+          ),
+          {
+            rank: false,
+          },
+        ),
+        batch: {
+          multicall: {
+            batchSize: 1024 * 200, /* cur.id === ChainId.POLYGON_ZKEVM ? 128 : 1024 * 200, */
+            wait: 16,
+          },
+        },
+        pollingInterval: 6_000,
+      }),
+    }
+  }, {} as Record<ChainId, PublicClient>)
+}
+
+export const viemClients = createViemPublicClients()
+
+export const getViemClients = createViemPublicClientGetter({ viemClients })
+
+type CreateViemPublicClientGetterParams = {
+  viemClients?: Record<ChainId, PublicClient>
+} & CreatePublicClientParams
+
+export function createViemPublicClientGetter({
+  viemClients: viemClientsOverride,
+  ...restParams
+}: CreateViemPublicClientGetterParams = {}) {
+  const clients = viemClientsOverride || createViemPublicClients(restParams)
+
+  return function getClients({ chainId }: { chainId?: ChainId }): PublicClient {
+    return clients[chainId as ChainId]
+  }
+}
+
+const PUBLIC_MAINNET = 'https://polygon-rpc.com'
+
+export const CLIENT_CONFIG = {
+  batch: {
+    multicall: {
+      batchSize: 1024 * 200,
+      wait: 16,
+    },
+  },
+  pollingInterval: 6_000,
+}
+
+export const publicClient = ({ chainId }: { chainId?: ChainId }) => {
+  if (chainId && viemClients[chainId]) {
+    return viemClients[chainId]
+  }
+  let httpString: string | undefined
+
+  if (process.env.NODE_ENV === 'test' && chainId === polygon.id) {
+    httpString = PUBLIC_MAINNET
+  } else {
+    httpString = chainId && first(PUBLIC_NODES[chainId]) ? first(PUBLIC_NODES[chainId]) : undefined
+  }
+
+  const chain = CHAINS.find((c) => c.id === chainId)
+  return createPublicClient({ chain, transport: http(httpString), ...CLIENT_CONFIG })
+}
