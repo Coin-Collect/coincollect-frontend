daada27cca55a57df96fbe920a37e4d53e99c8af
Kazim Akgul <kazimakgul@gmail.com>
2025-09-22 12:20:59 +0300

    Revert "Phising Warning Disabled"
    This reverts commit 5df8d20ba16010c3edb2468c56b209b7f75e6a07.

--- src/state/user/hooks/index.tsx
+++ src/state/user/hooks/index.tsx
@@ -71,7 +71,7 @@ export function usePhishingBannerManager(): [boolean, () => void] {
     dispatch(hidePhishingWarningBanner())
   }, [dispatch])
 
-  return [false, hideBanner]
+  return [showPhishingWarningBanner, hideBanner]
 }
 
 // Get user preference for exchange price chart
