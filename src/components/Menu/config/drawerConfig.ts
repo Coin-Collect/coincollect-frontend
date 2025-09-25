9a91f4074bf4a8790eea7ecfeaa08957252d1219
Kazim Akgul <kazimakgul@gmail.com>
2025-09-25 11:53:17 +0300

    Update drawerConfig.ts

--- src/components/Menu/config/drawerConfig.ts
+++ src/components/Menu/config/drawerConfig.ts
@@ -1,6 +1,7 @@
 import { MenuItemsType } from "@pancakeswap/uikit";
 
 
+<<<<<<< Updated upstream
 const drawerLinks: MenuItemsType[] = [
     {
         label: 'NFTs',
@@ -24,6 +25,94 @@ const drawerLinks: MenuItemsType[] = [
                 label: 'Marketplace',
                 href: 'https://market.coincollect.org/',
             },            
+=======
+const drawerLinks: MenuItemsType[] = [
+    {
+        label: 'Collections',
+        icon: 'NftIcon',
+        href: '/nfts/collections',
+    },
+    {
+        label: 'NFT Stake',
+        icon: 'PoolIcon',
+        href: '/nftpools',
+    },
+    {
+        label: 'Claim Rewards',
+        icon: 'PrizeIcon',
+        href: '/claim',
+    },
+    {
+        label: 'Games',
+        icon: 'TicketIcon',
+        href: '/games',
+    },
+    {
+        label: 'Marketplace',
+        icon: 'OpenNewIcon',
+        href: 'https://market.coincollect.org/',
+    },
+    {
+        label: 'Farms',
+        icon: 'FarmIcon',
+        href: '/farms',
+    },
+    {
+        label: 'Stake',
+        icon: 'EarnIcon',
+        href: '/pools',
+    },
+    {
+        label: 'Quest (Soon)',
+        icon: 'LightningIcon',
+        href: '/?q=soon',
+    },
+    {
+        label: 'Swap',
+        icon: 'SwapIcon',
+        href: '/swap',
+    },
+    {
+        label: 'Liquidity',
+        icon: 'SyncAltIcon',
+        href: '/liquidity',
+    },
+    {
+        label: 'More',
+        icon: 'InfoIcon',
+        href: '/more',
+        initialOpenState: false,
+        items: [
+            {
+                label: '.COLLECT Domain',
+                href: 'https://get.unstoppabledomains.com/collect/',
+            },
+            {
+                label: 'CoinMarketCap',
+                href: 'https://coinmarketcap.com/currencies/coincollect/',
+            },
+            {
+                label: 'CoinGecko',
+                href: 'https://www.coingecko.com/en/coins/coincollect',
+            },
+            {
+                label: 'OpenSea',
+                href: 'https://opensea.io/web3Community/created',
+            },
+            {
+                label: 'DappRadar',
+                href: 'https://dappradar.com/dapp/coincollect',
+            },
+            {
+                label: 'Docs & Guides',
+                href: 'https://docs.coincollect.org/',
+            },
+
+        ],
+    },
+
+];
+>>>>>>> Stashed changes
 
         ],
     },
