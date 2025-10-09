3a0dceb2ae2c3e17c72852ccc3caf235eef82a23
Kazim Akgul <kazimakgul@gmail.com>
2025-10-09 13:05:40 +0300

    Remove default meta tags from _app.tsx
    Deleted the import and usage of DEFAULT_META and associated meta tags from the Head component in _app.tsx. This streamlines the head content and removes default SEO and social sharing metadata.

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -25,7 +25,6 @@ import Providers from '../Providers'
 import GlobalStyle from '../style/Global'
 import React from 'react'
 import styled from 'styled-components'
-import { DEFAULT_META } from 'config/constants/meta'
 
 // This config is required for number formatting
 BigNumber.config({
@@ -90,16 +89,6 @@ function MyApp(props: AppProps) {
   return (
     <>
       <Head>
-        <title key="page:title">{DEFAULT_META.title}</title>
-        <meta key="page:description" name="description" content={DEFAULT_META.description} />
-        <meta key="page:og:title" property="og:title" content={DEFAULT_META.title} />
-        <meta key="page:og:description" property="og:description" content={DEFAULT_META.description} />
-        <meta key="page:og:image" property="og:image" content={DEFAULT_META.image} />
-        <meta key="page:og:type" property="og:type" content="website" />
-        <meta key="page:twitter:card" name="twitter:card" content="summary_large_image" />
-        <meta key="page:twitter:title" name="twitter:title" content={DEFAULT_META.title} />
-        <meta key="page:twitter:description" name="twitter:description" content={DEFAULT_META.description} />
-        <meta key="page:twitter:image" name="twitter:image" content={DEFAULT_META.image} />
         <meta
           key="app:viewport"
           name="viewport"
