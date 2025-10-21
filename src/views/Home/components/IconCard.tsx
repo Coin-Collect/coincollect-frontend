9324dbc421307f7a2c33fb7c32bbf9d2ce72d0e6
Kazim Akgul <kazimakgul@gmail.com>
2025-10-21 22:23:29 +0300

    Add superhero video backgrounds to IconCards
    Introduces 9 superhero .webm videos and utility functions to select random videos. Updates IconCard and MetricsSection to display unique video backgrounds for each card, enhancing visual appeal and engagement.

--- src/views/Home/components/IconCard.tsx
+++ src/views/Home/components/IconCard.tsx
@@ -2,14 +2,32 @@ import { ReactNode } from 'react'
 import styled from 'styled-components'
 import { Card, CardBody, Box, CardProps } from '@pancakeswap/uikit'
 
-const StyledCard = styled(Card)<{ background?: string; rotation?: string }>`
+const StyledCard = styled(Card)<{ background?: string; rotation?: string; $hasVideo?: boolean }>`
   height: fit-content;
   padding: 1px 1px 4px 1px;
   box-sizing: border-box;
+  position: relative;
+  overflow: hidden;
 
   ${({ theme }) => theme.mediaQueries.md} {
     ${({ rotation }) => (rotation ? `transform: rotate(${rotation});` : '')}
   }
+`
+
+const VideoBackground = styled.video`
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+  z-index: 0;
+  opacity: 0.7;
+`
+
+const CardBodyWithVideo = styled(CardBody)`
+  position: relative;
+  z-index: 1;
 `
 
 const IconWrapper = styled(Box)<{ rotation?: string }>`
@@ -31,15 +49,25 @@ export interface IconCardData {
   background?: string
   borderColor?: string
   rotation?: string
+  videoSrc?: string
 }
 
-const IconCard: React.FC<IconCardProps> = ({ icon, background, borderColor, rotation, children, ...props }) => {
+const IconCard: React.FC<IconCardProps> = ({ icon, background, borderColor, rotation, videoSrc, children, ...props }) => {
   return (
-    <StyledCard background={background} borderBackground={borderColor} rotation={rotation} {...props}>
-      <CardBody>
+    <StyledCard background={background} borderBackground={borderColor} rotation={rotation} $hasVideo={!!videoSrc} {...props}>
+      {videoSrc && (
+        <VideoBackground
+          autoPlay
+          loop
+          muted
+          playsInline
+          src={videoSrc}
+        />
+      )}
+      <CardBodyWithVideo>
         <IconWrapper rotation={rotation}>{icon}</IconWrapper>
         {children}
-      </CardBody>
+      </CardBodyWithVideo>
     </StyledCard>
   )
 }
