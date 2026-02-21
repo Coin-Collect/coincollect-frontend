f8a23265d02f2a7ad9637f012cc7205dbef5323a
Kazim Akgul <kazimakgul@gmail.com>
2026-02-21 18:53:25 +0300

    Diagnose NFT mint unauthorized error

--- packages/uikit/src/components/CakePrice/CakePrice.tsx
+++ packages/uikit/src/components/CakePrice/CakePrice.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import styled from "styled-components";
+import styled, { keyframes } from "styled-components";
 import LogoRound from "../Svg/Icons/LogoRound";
 import Text from "../Text/Text";
 import Skeleton from "../Skeleton/Skeleton";
@@ -9,6 +9,32 @@ export interface Props {
   color?: keyof Colors | string;
   cakePriceUsd?: number;
 }
+
+const livePulse = keyframes`
+  0% {
+    transform: scale(0.9);
+    opacity: 0.65;
+    filter: drop-shadow(0 0 0 rgba(83, 222, 233, 0));
+  }
+  50% {
+    transform: scale(1.15);
+    opacity: 1;
+    filter: drop-shadow(0 0 6px rgba(83, 222, 233, 0.6));
+  }
+  100% {
+    transform: scale(0.9);
+    opacity: 0.65;
+    filter: drop-shadow(0 0 0 rgba(83, 222, 233, 0));
+  }
+`;
+
+const AnimatedLogoRound = styled(LogoRound)`
+  > circle:first-of-type {
+    transform-box: fill-box;
+    transform-origin: center;
+    animation: ${livePulse} 1.35s ease-in-out infinite;
+  }
+`;
 
 const PriceLink = styled.a`
   display: flex;
@@ -29,7 +55,7 @@ const CakePrice: React.FC<Props> = ({ cakePriceUsd, color = "textSubtle" }) => {
       href="/swap?outputCurrency=0x56633733fc8BAf9f730AD2b6b9956Ae22c6d4148"
       target="_blank"
     >
-      <LogoRound width="24px" mr="8px" />
+      <AnimatedLogoRound width="24px" mr="8px" />
       <Text color={color} bold>{`$${cakePriceUsd.toFixed(4)}`}</Text>
     </PriceLink>
   ) : (
